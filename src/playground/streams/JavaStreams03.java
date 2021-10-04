9d7107224e4e6f0e6c03d36999491438d43f96e4
Graeme Geldenhuys <graemeg@gmail.com>
2021-10-04 23:26:55 +0100

    Clean up examples 3 and 4.

--- src/playground/streams/JavaStreams03.java
+++ src/playground/streams/JavaStreams03.java
@@ -1,23 +1,17 @@
 package playground.streams;
 
-import java.lang.String;
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.*;
-import java.util.*;
-import java.nio.*;
-import java.io.IOException;
+import java.util.stream.IntStream;
 
 // 3. Integer Stream with sum
 public class JavaStreams03 {
-	public static void main(String[] args) throws IOException {
-		
+	public static void main(String[] args) {
+
 		System.out.println(
-			IntStream			// IntStream result is an argument to println() 
-				.range(1, 5)	// integers 1-4, with 5 exclusive
-				.sum());		// sum all the numbers
+			IntStream			// IntStream result is an argument to println()
+					.range(1, 5)	// integers 1-4, with 5 exclusive
+					.sum());		// sum all the numbers
 		System.out.println();
 	}
 }
 
-// jedit settings  :elasticTabstops=false:encoding=UTF-8:
+// jedit settings :elasticTabstops=false:encoding=UTF-8:
--- src/playground/streams/JavaStreams04.java
+++ src/playground/streams/JavaStreams04.java
@@ -1,22 +1,17 @@
 package playground.streams;
 
-import java.lang.String;
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.*;
-import java.util.*;
-import java.nio.*;
 import java.io.IOException;
+import java.util.stream.Stream;
 
-// 4. Stream.of, sortted and findFirst
+// 4. Stream.of, sorted and findFirst
 public class JavaStreams04 {
 	public static void main(String[] args) throws IOException {
 		Stream.of("Ava", "Aneri", "Alberto")	// create a stream from these string values
-			.sorted()	// sort them
-			.findFirst()	// find the first one only
-			.ifPresent(x -> System.out.println(x));	// if a value exists, print it
+				.sorted()	// sort them
+				.findFirst()	// find the first one only
+				.ifPresent(x -> System.out.println(x));	// if a value exists, print it
 		System.out.println();
 	}
 }
 
-// jedit settings  :elasticTabstops=false:encoding=UTF-8:
+// jedit settings :elasticTabstops=false:encoding=UTF-8:
